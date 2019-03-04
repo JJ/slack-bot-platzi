9e75f4d5e64a6159073dff7c423491e237ab3ce4
JJ Merelo <jjmerelo@gmail.com>
2019-03-04 22:00:07 +0100

    Montando el cliente

---
+++ rabbitmq/cliente.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import pika
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='..')
+credentials = pika.PlainCredentials('platzi', os.environ.get('RMQ_PASS'))
+
+parameters= pika.URLParameters('amqp://platzi:{}@localhost:5672/platzi'.format(os.environ.get('RMQ_PASS')))
+
+connection = pika.BlockingConnection( parameters )
+
+channel = connection.channel()
+
+channel.queue_declare(queue='platzi')
+
+channel.basic_publish(exchange='',
+                      routing_key='platzi',
+                      body='Descarga')
+
+print(" [x] Solicitada descarga")
+connection.close()
