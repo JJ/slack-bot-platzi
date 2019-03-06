91f16f7dee59d047ab02bcdc7db3fed7ba070a78
JJ Merelo <jjmerelo@gmail.com>
2019-03-06 09:43:10 +0100

    Añade función desde JS

---
+++ registra-desde-node.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var url = 'amqp://platzi:' + process.env.RMQ_PASS + "@localhost:5672/platzi";
+var celery = require('node-celery'),
+	client = celery.createClient({
+		CELERY_BROKER_URL: url,
+		CELERY_RESULT_BACKEND: url
+	});
+
+client.on('error', function(err) {
+	console.log(err);
+});
+
+client.on('connect', function() {
+    var comandos=['uno', 'uno', 'tres', 'uno', 'tres', 'uno', 'dos' ];
+    comandos.forEach( function (valor ) {
+	client.call('RegistraComandos.registra', [valor], function(result) {
+	    console.log(result);
+	    client.end();
+	});
+    });
+
+});
