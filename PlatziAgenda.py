a5842e39ca58db10bb80295c65ff921f9a627e01
JJ Merelo <jjmerelo@gmail.com>
2019-03-13 16:16:55 +0100

    Corrige error con comentarios

--- PlatziAgenda.py
+++ PlatziAgenda.py
@@ -1,12 +1,10 @@
 import os, json, re
 
 class PlatziAgenda:
-    """Una clase para contener los elementos de la agenda de la web de Platzi"""
+# 'Una clase para contener los elementos de la agenda de la web de Platzi
 
     def __init__ (self,  data_file = "data/cursos.json"):
-    """
-    Inicializa con el nombre del fichero extraído por scraping
-    """
+#  Inicializa con el nombre del fichero extraído por scraping
         if not os.path.exists( data_file ):
             data_file = "../" + data_file
 
@@ -15,27 +13,17 @@ class PlatziAgenda:
             self.primero = sorted(self.agenda.keys())[0]
 
     def siguiente(self):
-    """
-    Devuelve el curso siguiente, buscando por índice
-    """
+#    Devuelve el curso siguiente, buscando por índice
         return self.agenda[self.primero]
 
-    def codigos(self):
-    """
-    Devuelve todos los códigos que hay ahora mismo
-    """
+    def codigos(self): # Devuelve todos los códigos que hay ahora mismo
         return list(self.agenda.keys())
 
-    def curso(self, codigo ):
-    """ 
-    Devuelve el curso correspondiente al código
-    """
+    def curso(self, codigo ): #  Devuelve el curso correspondiente al código
         return self.agenda[codigo]
 
     def busca(self, aguja ):
-    """
-    Devuelve los cursos que incluyen esa cadena en el título, indexados por código de curso, o "Resultado: None" en caso de que no haya ninguno.
-    """
+#  Devuelve los cursos que incluyen esa cadena en el título, indexados por código de curso, o "Resultado: None" en caso de que no haya ninguno.
         resultado = {}
         for k, v in self.agenda.items():
             if v['titulo'].lower().find( aguja.lower() ) >= 0:
@@ -45,7 +33,6 @@ class PlatziAgenda:
             resultado = { "Encontrado": None }
 
         return resultado
-
 
 
 
